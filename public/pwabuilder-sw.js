041ff6050b7d6c8f80cc83b669e8b5b0682c999e
Joshua Lowe <joshualowe1002@gmail.com>
2020-11-01 18:50:43 +0000

    Cache pages

--- public/pwabuilder-sw.js
+++ public/pwabuilder-sw.js
@@ -1,11 +1,11 @@
-// This is the "Offline page" service worker
+// This is the service worker with the combined offline experience (Offline page + Offline copy of pages)
+
+const CACHE = "pwabuilder-offline-page";
 
 importScripts("./workbox-sw.js");
 
-const CACHE = "pwabuilder-page";
-
 // TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
-const offlineFallbackPage = "offline.html";
+const offlineFallbackPage = "ToDo-replace-this-name.html";
 
 self.addEventListener("message", event => {
   if (event.data && event.data.type === "SKIP_WAITING") {
@@ -22,6 +22,13 @@ self.addEventListener("install", async event => {
 if (workbox.navigationPreload.isSupported()) {
   workbox.navigationPreload.enable();
 }
+
+workbox.routing.registerRoute(
+  new RegExp("/*"),
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: CACHE
+  })
+);
 
 self.addEventListener("fetch", event => {
   if (event.request.mode === "navigate") {
